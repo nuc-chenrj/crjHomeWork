423036ae966a31b91167f8b553f87b955fbba56b
nuc-chenrj <1412736935@qq.com>
2020-06-18 17:12:11 +0800

    陈瑞杰作业

---
+++ src/dy04/Arr.java
@@ -0,0 +1,46 @@
+package dy04;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Arr {
+
+    public static void main(String[] args) {
+        int n=8;
+        int[] arr=new int [8];
+        Scanner sc=new Scanner(System.in);
+        int a=0,b=0;
+        for(int i=0;i<n;i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i]%2==0)a++;
+            else b++;
+        }
+        int[] arrEven=new int[a];
+        int[] arrOdd=new int[b];
+        int indexEven=0,indexOdd=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==0)arrEven[indexEven++]=arr[i];
+            else arrOdd[indexOdd++]=arr[i];
+        }
+        Arrays.sort(arrEven);
+        Arrays.sort(arrOdd);
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arrEven));
+//        System.out.println(Arrays.toString(arrOdd));
+        int[] ans=f(arrEven,arrOdd);
+        System.out.println(Arrays.toString(ans));
+
+    }
+    public static int[] f(int[] a, int[] b){
+        int an=a.length,bn=b.length;
+        int[] ans=new int[an+bn];
+        int i=0,j=0,index=0;
+        while(i<an && j<bn){
+            ans[index++]=a[i++];
+            ans[index++]=b[j++];
+        }
+        while(i<an)ans[index++]=a[i++];
+        while(j<bn)ans[index++]=b[j++];
+        return ans;
+    }
+}
--- src/dy04/Game.java
+++ src/dy04/Game.java
@@ -4,10 +4,27 @@ import java.util.Arrays;
 
 public class Game {
     public static void main(String[] args) {
+        char[] input={'L','B','W','N','P'};
         char[] chars=generate();
         System.out.println(Arrays.toString(chars));
+        int[] ans=check(chars,input);
+        System.out.println(Arrays.toString(ans));
     }
 
+    public static int[] check(char[] chars,char[] input){
+        int n=input.length;
+        int[] ans=new int[2];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(chars[i]==input[j]){
+                    ans[0]++;
+                    if(i==j) ans[1]++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
     public static char[] generate(){
         char[] chs=new char[5];
         char[] letters = {'A','B','C','D','E','F','G','H','I','J','K','L','M'
