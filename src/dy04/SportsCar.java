6a55b3743fb32f00284d1bd522e0bef4dc3b4487
nuc-chenrj <1412736935@qq.com>
2020-06-18 10:06:29 +0800

    陈瑞杰作业

---
+++ src/dy04/SportsCar.java
@@ -0,0 +1,22 @@
+package dy04;
+
+public class SportsCar extends Car{
+
+
+    public SportsCar(String brand) {
+        super(brand);
+    }
+
+    public SportsCar() {
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println("超级跑车");
+    }
+
+    public void price(){
+        System.out.println("售价100w");
+    }
+}
---
+++ src/dy04/Test.java
@@ -0,0 +1,17 @@
+package dy04;
+
+public class Test {
+    public static void main(String[] args) {
+        SportsCar car1= new SportsCar("红旗");
+        System.out.println(car1.getBrand());
+        car1.run();
+        car1.price();
+
+        Car car2=new SportsCar();
+        car2.setBrand("宝马");
+        System.out.println(car2.getBrand());
+        car2.run();
+        //
+        //((SportsCar)car2).price();
+    }
+}
