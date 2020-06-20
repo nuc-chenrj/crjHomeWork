6f3961803a7f4e1964abbf3d57f422db68fe65f6
nuc-chenrj <1412736935@qq.com>
2020-06-20 16:44:32 +0800

    陈瑞杰作业

--- src/dy06/Copy2.java
+++ src/dy06/Copy2.java
@@ -2,20 +2,19 @@ package dy06;
 
 import java.io.*;
 
-public class Copy {
+public class Copy2 {
     public static void main(String[] args) {
 
-        PrintWriter pw = null;
         BufferedReader br = null;
+        BufferedWriter bw = null;
         try {
-            FileOutputStream fos = new FileOutputStream("b.txt");
-            OutputStreamWriter osw = new OutputStreamWriter(fos);
-            pw = new PrintWriter(osw, true);
-            FileInputStream fis = new FileInputStream("a.txt");
-            InputStreamReader isr = new InputStreamReader(fis);
-            br = new BufferedReader(isr);
+            br=new BufferedReader(new FileReader("a.txt"));
+            bw = new BufferedWriter(new FileWriter("b.txt"));
             String str;
-            while ((str = br.readLine()) != null) pw.println(str);
+            while ((str = br.readLine()) != null) {
+                bw.write(str);
+                bw.newLine();
+            }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -24,7 +23,7 @@ public class Copy {
         } finally {
             try {
                 if (br != null) br.close();
-                if (pw != null) pw.close();
+                if (bw != null) bw.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
