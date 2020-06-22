20e66a7995bded35663f06a2d2e70a98dccc9e3b
nuc-chenrj <1412736935@qq.com>
2020-06-22 15:43:36 +0800

    陈瑞杰作业

---
+++ src/dy07/PrintDate.java
@@ -0,0 +1,26 @@
+package dy07;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class PrintDate {
+    public static void main(String[] args) {
+        new Thread(){
+            public void run(){
+                while(true){
+                    Date date=new Date();
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    String now=sdf.format(date);
+                    System.out.println(now);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.run();
+
+    }
+}
